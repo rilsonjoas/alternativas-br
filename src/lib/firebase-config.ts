ddd19d67de20a2eab70e31ae46b6773697d7ee1f
Rilson Joás <rilsonjoas10@gmail.com>
2025-09-06 17:49:32 -0300

    Configuração do Vercel

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -2,12 +2,12 @@
 // As variáveis são carregadas do arquivo .env.local
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBiUNvJc-sBHkjYSpgqeOqtabDcC3wbb08",
-  authDomain: "alternativas-br.firebaseapp.com",
-  projectId: "alternativas-br",
-  storageBucket: "alternativas-br.firebasestorage.app",
-  messagingSenderId: "980293963848",
-  appId: "1:980293963848:web:e1e7e0722b4197565e487d",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBiUNvJc-sBHkjYSpgqeOqtabDcC3wbb08",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "alternativas-br.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "alternativas-br",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "alternativas-br.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "980293963848",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:980293963848:web:e1e7e0722b4197565e487d",
   measurementId: "G-7FTVYGNH79"
 };
 
